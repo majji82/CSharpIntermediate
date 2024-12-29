b231caeecacab012e476de5d5af623cafc363eb3
majji82 <2100032153cseh@gmail.com>
2024-12-29 16:27:09 +0530

    merged into a folder

---
+++ AssosciationBetweenClasses/Composition/Composition/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Composition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DBMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+            installer.Install();
+        }
+    }
+}
---
+++ AssosciationBetweenClasses/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            text.Width = 100;
+            text.Height = 100;
+            text.FontName = "Arial";
+            text.FontSize = 16;
+
+            text.Copy();
+
+            Console.WriteLine("Hello");
+        }
+    }
+}
