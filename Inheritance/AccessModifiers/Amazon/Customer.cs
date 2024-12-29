136ecba80387b3b265d5598162a639f5e5d630ba
majji82 <2100032153cseh@gmail.com>
2024-12-29 18:47:34 +0530

    done with access modifiers

---
+++ Inheritance/AccessModifiers/Amazon/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace Amazon
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var calculator = new RateCalculator();
+            var rating = calculator.Calculate(this);
+            Console.WriteLine("Promoted");
+        }
+
+    }
+}
